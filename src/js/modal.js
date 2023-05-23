5b40b483ea8c1de6e66498d1e03a6e40f7c82e4d
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2023-05-23 15:55:31 +0200

    peurdate

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,3 +1,6 @@
+!function(e){"function"!=typeof e.matches&&(e.matches=e.msMatchesSelector||e.mozMatchesSelector||e.webkitMatchesSelector||function(e){for(var t=this,o=(t.document||t.ownerDocument).querySelectorAll(e),n=0;o[n]&&o[n]!==t;)++n;return Boolean(o[n])}),"function"!=typeof e.closest&&(e.closest=function(e){for(var t=this;t&&1===t.nodeType;){if(t.matches(e))return t;t=t.parentNode}return null})}(window.Element.prototype);
+
+
 document.addEventListener('DOMContentLoaded', function() {
 
    /* Записываем в переменные массив элементов-кнопок и подложку.
@@ -21,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function() {
          /* При каждом клике на кнопку мы будем забирать содержимое атрибута data-modal
             и будем искать модальное окно с таким же атрибутом. */
          var modalId = this.getAttribute('data-modal'),
-             modalElem = document.querySelector('.modal-th[data-modal="' + modalId + '"]');
+             modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
 
 
          /* После того как нашли нужное модальное окно, добавим классы
@@ -36,7 +39,7 @@ document.addEventListener('DOMContentLoaded', function() {
    closeButtons.forEach(function(item){
 
       item.addEventListener('click', function(e) {
-         var parentModal = this.closest('.modal-th');
+         var parentModal = this.closest('.modal');
 
          parentModal.classList.remove('active');
          overlay.classList.remove('active');
@@ -50,14 +53,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
         if (key == 27) {
 
-            document.querySelector('.modal-th.active').classList.remove('active');
+            document.querySelector('.modal.active').classList.remove('active');
             document.querySelector('.overlay').classList.remove('active');
         };
     }, false);
 
 
     overlay.addEventListener('click', function() {
-        document.querySelector('.modal-th.active').classList.remove('active');
+        document.querySelector('.modal.active').classList.remove('active');
         this.classList.remove('active');
     });
 
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+
+import './js/mobile-menu';
 import './js/modal-fruits';
 import './js/modal';
-import './js/mobile-menu';
